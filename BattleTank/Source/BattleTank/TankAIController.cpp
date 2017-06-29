f03f20bc44bce5a175544f6dace63e0a7ab59745
DESKTOP-3IMCUUV\diego <diegomare75@gmail.com>
2017-06-29 18:20:29 +0200

    ai controlled tanks setup with find player tank

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: AI Controlled Tank not found!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: AI Controlled Tank = %s"), *(ControlledTank->GetName()));
+	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: Player Controlled Tank not found!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: Player Controlled Tank = %s"), *(PlayerTank->GetName()));
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if(!PlayerPawn)	{ return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
