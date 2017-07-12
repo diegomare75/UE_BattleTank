f162008b83b3f779e17dcc226ab836e3d519bb6f
DESKTOP-3IMCUUV\diego <diegomare75@gmail.com>
2017-07-12 20:37:23 +0200

    reload speed

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -8,52 +8,26 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: AI Controlled Tank not found!"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: AI Controlled Tank = %s"), *(ControlledTank->GetName()));
-	}
-
-	auto PlayerTank = GetPlayerTank();
-	if (!PlayerTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: Player Controlled Tank not found!"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: Player Controlled Tank = %s"), *(PlayerTank->GetName()));
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController MSG: Player Controlled Tank = %s"), *(PlayerTank->GetActorLocation().ToString()));
-	}
 }
 
 void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	if (GetPlayerTank())
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+
+	auto ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank)
 	{
-		// move towards the player
+		// TODO move towards the player
 
 		// aim the player
-		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
+		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
 		// fire when ready
+		ControlledTank->Fire();
 	}
 }
 
-ATank* ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
-}
 
-ATank * ATankAIController::GetPlayerTank() const
-{
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if(!PlayerPawn)	{ return nullptr; }
-	return Cast<ATank>(PlayerPawn);
-}
